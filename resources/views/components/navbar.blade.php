14ba700c8b3fa27872ed64f414611d04f6c6a199
Emiddio <ricco_emiddio@alice.it>
2024-10-21 16:07:33 +0200

    creazione pagina contatti

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,7 +13,7 @@
             <a class="nav-link" href="{{ route('about-us') }}">aboutus</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="{{ route('contatact.us') }}">contattaci</a>
           </li>
           <li class="nav-item">
             <a class="nav-link disabled">Disabled</a>
--- resources/views/contattaci.blade.php
+++ resources/views/contattaci.blade.php
@@ -1,5 +1,20 @@
 <x-layout>
     {{-- <h1>(Route::CurrentRouteName())</h1> --}}
-        <x-navbar />
-<h1>contattaci</h1>
+    <x-navbar />
+
+    <div class="row justify-content-center">
+        <div class="class col-12 col-md-6">
+            <form class="p-5 bg-primary shadow rounded">
+                <div class="mb-3">
+                    <label for="username" class="form-label">inserisci email</label>
+                    <input type="text" class="form-control" id="username">
+                </div>
+                <div class="mb-3">
+                    <textarea name="" id="message" class="form-control" cols="30" rows="10"></textarea>
+                    <label for="message" class="form-label">scrivici</label>
+                </div>
+                <button type="submit" class="btn btn-danger">Submit</button>
+            </form>
+        </div>
+    </div>
 </x-layout>
